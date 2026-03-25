0468e1370b8dac8d6397d9586e83010f7e173418
Edmund Managuit <edmund.managuit@gmail.com>
2026-03-25 11:10:03 +0800

    add new report Supplier Balance per Invoice

---
+++ ttc_reports/ttc_reports/report/supplier_balance_per_invoice/supplier_balance_per_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Edmund Managuit and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Supplier Balance per Invoice"] = {
+	"filters": [
+
+	]
+};
